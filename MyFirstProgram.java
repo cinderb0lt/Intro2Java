6db8d04f07f608aa241c94df0650e4b17e9d0271
Freya Matthews <41971153+cinderb0lt@users.noreply.github.com>
2018-11-27 22:32:08 -0500

    Add files via upload

---
+++ MyFirstProgram.java
@@ -0,0 +1,8 @@
+
+public class MyFirstProgram { //Declares the class "My first program"
+    public static void main(String[] args) {
+        System.out.println("A One"); //Prints "A One" on a new line
+        System.out.println("Two"); //Prints "Two" on a new line
+
+    } //Curley braces should match up
+}
